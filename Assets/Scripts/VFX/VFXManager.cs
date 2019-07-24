1556aa6388aced4ea5ce6ab918ef5ce86224300a
Danial <danial.kordmodanlou@gmail.com>
2019-07-24 12:04:47 -0700

    vfx
    visual effect for spawning added

---
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFXManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private VisualEffect visualEffect;
+    
+    [SerializeField,Range(0.1f,10)] private float size;
+    [SerializeField] private Gradient color;
+    void Start()
+    {
+        this.visualEffect = this.GetComponent<VisualEffect>();
+        color = visualEffect.GetGradient("color");
+        size = visualEffect.GetFloat("size");
+    }
+
+    public VFXManager(VisualEffect visualEffect)
+    {
+        this.visualEffect = visualEffect;
+        color = visualEffect.GetGradient("color");
+        size = visualEffect.GetFloat("size");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(visualEffect.enabled)
+            setSize(getSize()-0.0005f);
+
+    }
+
+    public void setSize(float size)
+    {
+        if (size < 0)
+        {
+            visualEffect.enabled = false;
+            return;
+        }
+           
+        visualEffect.SetFloat("size",size);
+        this.size = size;
+    }
+
+    public void setColor(Gradient gradient)
+    {
+        visualEffect.SetGradient("color",gradient);
+    }
+
+    public float getSize()
+    {
+        return size;
+    }
+}
---
+++ Assets/Scripts/movment/movment.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+public class movment : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private CharacterController characterController;
+    private Animator animator;
+    [SerializeField] private VisualEffect portal;
+    private bool attack = false;
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Start()
+    {
+        Instantiate(portal, new Vector3(transform.position.x,0.39f,transform.position.z),portal.transform.rotation);
+        //  Invoke("destroyPortal",4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizantal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+
+        var movment = this.transform.forward * vertical;
+
+        characterController.SimpleMove(movment*3);
+
+        this.transform.Rotate(new Vector3(0,horizantal*4,0));
+
+        animator.SetFloat("speed",vertical);
+
+        if (Input.GetKeyDown(KeyCode.E) && !attack)
+        {
+            attack = true;
+            Invoke("attacked", 1f);
+            animator.SetTrigger("attack");
+        } 
+        
+    }
+
+    private void destroyPortal()
+    {
+        portal.GetComponent<VisualEffect>().enabled = false;
+    }
+    private void attacked()
+    {
+        attack = false;
+    }
+}
